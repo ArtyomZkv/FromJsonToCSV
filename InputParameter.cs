9e57e6a3ceaa203cb1e53137b1d16110cf2bd3ba
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 18:08:18 +0300

    в данном классе хранятся данные для ввода параметра пути записи .json файла

---
+++ InputParameter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FromJsonToCSV
+{
+    static class InputParameter
+    {
+        private const string _secondCharOfInputIndex = "i";
+        static public string SecondCharOfInputIndex { get { return _secondCharOfInputIndex; } }
+    }
+}
