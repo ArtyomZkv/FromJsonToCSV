d8ae988324f577055204bef4ed8b72647659ecf8
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 18:09:40 +0300

    В данном классе хранится ключ для параметра -q

---
+++ KeyParameter.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FromJsonToCSV
+{
+    static class KeyParameter
+    {
+        static private readonly string _key = "131499a8-998e-477c-bae3-906baddd8dc8";
+        static private readonly string _secondCharOfKeyParameter = "q";
+        static public string Key { get { return _key; } }
+        static public string SecondCharOfKeyParameter { get { return _secondCharOfKeyParameter; } }
+        
+    }
+}
