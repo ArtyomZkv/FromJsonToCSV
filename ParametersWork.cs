01d0f8629d84903371ff4089d7189dbbe4670b67
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 18:10:33 +0300

    в данном классе хранятся данные для работы с первым символом вводимых параметров

---
+++ ParametersWork.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FromJsonToCSV
+{
+    static class ParametersWork
+    {
+        private static readonly string _firstCharOfAnyParameter = "-";
+
+        public static string FirstCharOfAnyParameter
+        {
+            get
+            {
+                return _firstCharOfAnyParameter;
+            }
+        }
+
+    }
+}
