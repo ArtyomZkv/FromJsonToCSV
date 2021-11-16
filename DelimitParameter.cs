da21d604399ad1d244a70d9d3b102acac1c1bc1a
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 17:55:31 +0300

    В данном классе хранятся свойства, в которые записываются используемые директории при работе с файлами

---
+++ DelimitParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromJsonToCSV
+{
+
+    static class DelimitParameter
+    {
+        static private string _secondCharForDelimeter = "s";
+        static public string SecondCharForDelimeter { get { return secondCharForDelimeter; } }
+        static public string Delimeter { get; set; }
+    }
+}
---
+++ PathKeeper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace FromJsonToCSV
+{
+    class PathKeeper
+    {
+        public static string InputPath { get; set; }
+        public static string OutputPath { get; set; }        
+    }
+}
