84472bb2b83331205723fbab58a32574d01ac0e1
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 18:09:14 +0300

    данный класс отвечает за логику написания .json файла

---
+++ JsonFileWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+namespace FromJsonToCSV
+{
+    class JsonFileWork
+    {
+        public static void CreateJsonFile(List<DataForJsonFile> list, string path)
+        {
+            string json = JsonSerializer.Serialize(list);
+            File.WriteAllText(path, json);
+        }
+    }
+}
