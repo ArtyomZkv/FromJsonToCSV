228cf90b0557f414385a05d6870342f4aa1cd1eb
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-16 18:09:57 +0300

    в данном классе хранятся данные для ввода параметра пути записи .csv файла

---
+++ OutputParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromJsonToCSV
+{
+    static class OutputParameter
+    {
+        private static readonly string _secondCharOfOutputIndex = "o";
+        static public string SecondCharOfOutputIndex { get { return _secondCharOfOutputIndex; } }
+        static public void FindCurrentOutputParameter(string arg)
+        {
+            if (arg.IndexOf(ParametersWork.FirstCharOfAnyParameter) != 0 && arg.IndexOf(SecondCharOfOutputIndex) != 1)
+            {
+                PathKeeper.OutputPath = arg;
+            }
+        }
+    }
+}
